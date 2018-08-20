bcccb52b4ff39ded674eaf2832d4a6948939d960
Dan Kni <me@danknight.name>
2018-08-20 21:14:54 +0100

    First commit

---
+++ imgs.js
@@ -0,0 +1,371 @@
+var climbImgs = {      
+"imgs":[
+ {
+   "climbId": 1,
+   "type": "crag",
+   "url": "img/tiles/The-Old-Man-of-Stoer-climb.jpg",
+   "alt": "The Sea Stack Know as The Old Man of Stoer",
+   "atributionURL": "https://commons.wikimedia.org/wiki/File:2011_Schotland_The_Old_Man_of_Stoer_30-05-2011_15-32-59.png",
+   "attributionText": "image source"
+ },
+ {
+   "climbId": 1,
+   "type": "topo",
+   "url": "img/topos/stoer/old-man-of-stoer-topo.jpg",
+   "alt": "Topo for The Old Man of Stoer",
+   "atributionURL": "https://commons.wikimedia.org/wiki/File:Old_man_of_stoer.jpg",
+   "attributionText": "original image"
+ },
+ {
+   "climbId": 1,
+   "type": "map",
+   "url": "img/maps/old-man-of-stoer-map.png",
+   "alt": "The Old Man of Stoer's Location in Scotland",
+   "atributionURL": "http://google.com/maps",
+   "attributionText": "Google Map"
+ },
+ {
+   "climbId": 2,
+   "type": "crag",
+   "url": "img/topos/bosigran/Bosigran-sea-cliff.jpg",
+   "alt": "Bosigran's Sea Cliffs offer accessible rock climbing",
+   "atributionURL": "https://www.flickr.com/photos/ebygomm",
+   "attributionText": "image credit: Elizabeth Gomm"
+ },
+ {
+   "climbId": 2,
+   "type": "topo",
+   "url": "img/topos/bosigran/doorpost-topo.jpg",
+   "alt": "Bosigran's Doorpost Multipitch Route Topo",
+   "atributionURL": "http://seansmountaincamera.blogspot.com/2016/04/back-on-real-rock-cornwall-march-2016.html",
+   "attributionText": "original image source"
+ },
+ {
+   "climbId": 2,
+   "type": "map",
+   "url": "img/maps/bosigran.png",
+   "alt": "Bosigran's Location in Cornwall",
+   "atributionURL": "http://google.com/maps",
+   "attributionText": "Google Map"
+ },
+ {
+   "climbId": 3,
+   "type": "crag",
+   "url": "img/topos/cloggy/cloggy-crag.jpg",
+   "alt": "The crag at Clogwyn Du'r Arddu or Cloggy",
+   "atributionURL": "https://www.thebmc.co.uk/zoomtopo/cloggy/",
+   "attributionText": "view BMC super zoom"
+ },
+ {
+   "climbId": 3,
+   "type": "topo",
+   "url": "img/topos/cloggy/cloggy-topo-for-great-slab.jpg",
+   "alt": "The Great Slab Topo on Clogwyn Du'r Arddu",
+   "atributionURL": "https://www.thebmc.co.uk/zoomtopo/cloggy/",
+   "attributionText": "view BMC super zoom"
+ },
+ {
+   "climbId": 3,
+   "type": "map",
+   "url": "img/maps/cloggy-map-wales.png",
+   "alt": "Map showing the location of the great slab on Cloggy",
+   "atributionURL": "http://google.com/maps",
+   "attributionText": "Google Map"
+ },
+ {
+   "climbId": 5,
+   "type": "crag",
+   "url": "img/topos/cnoc/tormore-trio.jpg",
+   "alt": "Cnoc Na Mara Sea Stack",
+   "atributionURL": "https://uniqueascent.ie/cnoc_na_mara",
+   "attributionText": "Credit Iain Miller"
+ },
+ {
+   "climbId": 5,
+   "type": "topo",
+   "url": "img/topos/cnoc/cnoc-na-mara-route-topo.jpg",
+   "alt": "Cnoc Na Mara Route Topography",
+   "atributionURL": "https://uniqueascent.ie/cnoc_na_mara",
+   "attributionText": "Credit Iain Miller"
+ },
+ {
+   "climbId": 5,
+   "type": "map",
+   "url": "img/maps/tormore-group-map-ireland.png",
+   "alt": "Map showing the location of the Tormore Sea Stacks",
+   "atributionURL": "http://google.com/maps",
+   "attributionText": "Google Map"
+ },
+ {
+   "climbId": 6,
+   "type": "crag",
+   "url": "img/topos/sass/sass-pordoi-crag.jpg",
+   "alt": "The Amazing Alpine Crag of Sass Pordoig",
+   "atributionURL": "https://pixabay.com/en/sass-pordoi-sella-massif-2738859/",
+   "attributionText": "image credit"
+ },
+ {
+   "climbId": 6,
+   "type": "topo",
+   "url": "img/topos/sass/sass-pordoi-via-fedele-topo-crop.jpg",
+   "alt": "Sass Pordoi Route Via Fedele Topo",
+   "atributionURL": "https://pixabay.com/en/sass-pordoi-sella-massif-2738856/",
+   "attributionText": "original image source"
+ },
+ {
+   "climbId": 6,
+   "type": "map",
+   "url": "img/maps/sass-pordoi.png",
+   "alt": "Map showing Sass Pordoi In the Dolomites",
+   "atributionURL": "http://google.com/maps",
+   "attributionText": "Google Map"
+ },
+ {
+   "climbId": 7,
+   "type": "crag",
+   "url": "img/topos/idwal/idwal-slabs-crag.jpg",
+   "alt": "The easiest route up Cwm Idwal Slabs",
+   "atributionURL": "https://climbmountains.wordpress.com/2010/06/10/new-ogwen-climbing-guidebook-out-now/",
+   "attributionText": "image credit"
+ },
+ {
+   "climbId": 7,
+   "type": "topo",
+   "url": "img/topos/idwal/idwal-slabs-ordinary-route-topo.jpg",
+   "alt": "Idwal Slabs offers easy multi-picth climbing Topo",
+   "atributionURL": "https://www.ukclimbing.com/forums/rocktalk/idwal_slabs_-_ordinary_route-530108",
+   "attributionText": "owner unknown - referenced UKC"
+ },
+ {
+   "climbId": 7,
+   "type": "map",
+   "url": "img/maps/cwm-idwal.png",
+   "alt": "Map showing Cwm Idwal in Wales",
+   "atributionURL": "http://google.com/maps",
+   "attributionText": "Google Map"
+ },
+ {
+   "climbId": 8,
+   "type": "crag",
+   "url": "img/topos/lundy/devols-slide-on-lundy-island.jpg",
+   "alt": "The Devils slide On Lundy",
+   "atributionURL": "https://www.flickr.com/photos/geographyalltheway_photos/6843213186/in/photostream/",
+   "attributionText": "Image: Richard Allaway"
+ },
+ {
+   "climbId": 8,
+   "type": "topo",
+   "url": "img/topos/lundy/devils-slide-topo.jpg",
+   "alt": "The Devils Slide Route Topography",
+   "atributionURL": "https://www.pinterest.co.uk/tommacdonald714/",
+   "attributionText": "Original By Tom MacDonald"
+ },
+ {
+   "climbId": 8,
+   "type": "map",
+   "url": "img/maps/devils-slide-map-lundy.png",
+   "alt": "Map showing the location of the Tormore Sea Stacks",
+   "atributionURL": "http://google.com/maps",
+   "attributionText": "Google Map"
+ },
+ {
+   "climbId": 10,
+   "type": "crag",
+   "url": "img/topos/wreakers/wreakers-slab.jpg",
+   "alt": "The Amazing Sea Crag in Cornwall",
+   "atributionURL": "https://creativecommons.org/licenses/by/2.0/uk/",
+   "attributionText": "our own image"
+ },
+ {
+   "climbId": 10,
+   "type": "topo",
+   "url": "img/topos/wreakers/cornakey-cliff-wreakers-slab-topo-small.jpg",
+   "alt": "Wreakers Slab Topo on Cornakey Cliff",
+   "atributionURL": "https://creativecommons.org/licenses/by/2.0/uk/",
+   "attributionText": "our own image"
+ },
+ {
+   "climbId": 10,
+   "type": "map",
+   "url": "img/maps/cornakey-cliff-cornwall-map.png",
+   "alt": "Map showing Wreakers Slab on Cornakey Cliff",
+   "atributionURL": "http://google.com/maps",
+   "attributionText": "Google Map"
+ },
+ {
+   "climbId": 2,
+   "type": "tile",
+   "url": "img/tiles/bosigran-small.jpg",
+   "alt": "Bosigran's Sea Cliffs offer accessible rock climbing",
+   "atributionURL": "http://mountainmagic.org.uk/places/cornwall/",
+   "attributionText": ""
+ },
+ {
+   "climbId": 1,
+   "type": "tile",
+   "url": "img/tiles/old-man-of-stoer-small.jpg",
+   "alt": "The Sea Stack Know as The Old Man of Stoer",
+   "atributionURL": "",
+   "attributionText": ""
+ },
+ {
+   "climbId": 3,
+   "type": "tile",
+   "url": "img/tiles/cloggy-west-butress-small.jpg",
+   "alt": "The crag at Clogwyn Du'r Arddu or Cloggy",
+   "atributionURL": "",
+   "attributionText": ""
+ },
+ {
+   "climbId": 4,
+   "type": "tile",
+   "url": "img/tiles/vratsa-bulgaria-climbing.jpg",
+   "alt": "The Amazing Limestone Cliffs of Vratsa",
+   "atributionURL": "",
+   "attributionText": ""
+ },
+ {
+   "climbId": 5,
+   "type": "tile",
+   "url": "img/tiles/cnoc-na-mara-donegal.jpg",
+   "alt": "Cnoc Na Mara Sea Stack",
+   "atributionURL": "https://uniqueascent.ie/cnoc_na_mara",
+   "attributionText": ""
+ },
+ {
+   "climbId": 6,
+   "type": "tile",
+   "url": "img/tiles/fedele-on-sass-pordoi-in-the-alps.jpg",
+   "alt": "The Amazing Alpine Crag of Sass Pordoig",
+   "atributionURL": "",
+   "attributionText": ""
+ },
+ {
+   "climbId": 7,
+   "type": "tile",
+   "url": "img/tiles/cwm-idwal-slabs.jpg",
+   "alt": "The bowl of Cwm Idwal",
+   "atributionURL": "",
+   "attributionText": ""
+ },
+ {
+   "climbId": 8,
+   "type": "tile",
+   "url": "img/tiles/devils-slide-on-lundy.jpg",
+   "alt": "The Devils slide On Lundy",
+   "atributionURL": "https://www.flickr.com/photos/geographyalltheway_photos/6843213186",
+   "attributionText": ""
+ },
+ {
+   "climbId": 9,
+   "type": "tile",
+   "url": "img/tiles/meadinha-in-portugal.jpg",
+   "alt": "The Granite dome of Meadinha",
+   "atributionURL": "http://chinelodemeterodedo.com/escalada-classica-no-parque-nacional-da-peneda-geres-meadinha/",
+   "attributionText": ""
+ },
+ {
+   "climbId": 10,
+   "type": "tile",
+   "url": "img/tiles/wreakers-slab-small.jpg",
+   "alt": "Wreakers Slab Sea Cliff in Cornwall",
+   "atributionURL": "",
+   "attributionText": ""
+ },
+ {
+   "climbId": 11,
+   "type": "crag",
+   "url": "img/topos/cir-mhor/south-ridge-cir-mhor.jpg",
+   "alt": "The stunning Rosa Pinicle on Cir Mhor",
+   "atributionURL": "https://www.geograph.org.uk/profile/24401",
+   "attributionText": "img: Raibeart MacAoidh"
+ },
+ {
+   "climbId": 11,
+   "type": "topo",
+   "url": "img/topos/cir-mhor/south-ridge-direct-topo-maybe.jpg",
+   "alt": "The South Ridge Direct Route on Cir Mhor",
+   "atributionURL": "http://beantinblog.blogspot.com/2016/08/south-ridge-direct-cir-mhor-arran.html",
+   "attributionText": "original image"
+ },
+ {
+   "climbId": 11,
+   "type": "tile",
+   "url": "img/tiles/cir-mhor-ridge-arran.jpg",
+   "alt": "The South Ridge Direct Route on Cir Mhor",
+   "atributionURL": "https://www.geograph.org.uk/profile/24401",
+   "attributionText": "img: Raibeart MacAoidh"
+ },
+ {
+   "climbId": 12,
+   "type": "crag",
+   "url": "img/topos/montserrat/roca-gris-esparreguera-crag-montserrat.jpg",
+   "alt": "The Grey Rock in Montserrat",
+   "atributionURL": "https://elcoleccionistadevies.blogspot.com/2016/07/18-anys-de-laresta-esparreguera-la-roca.html",
+   "attributionText": "image source"
+ },
+ {
+   "climbId": 12,
+   "type": "topo",
+   "url": "img/topos/montserrat/roca-gris-esparreguera-topo.jpg",
+   "alt": "Topo for Esparreguera climb in Montserrat Spain",
+   "atributionURL": "https://joanasin.blogspot.com/2017/01/montserrat-roca-gris-via-esparraguera.html",
+   "attributionText": "original image"
+ },
+ {
+   "climbId": 12,
+   "type": "tile",
+   "url": "img/tiles/grey-rock-at-montserrat.jpg",
+   "alt": "Grey Rock at Montserrat",
+   "atributionURL": "",
+   "attributionText": ""
+ },
+ {
+   "climbId": 13,
+   "type": "tile",
+   "url": "img/topos/sail-rock/sail-rock-donegal-crag-s.jpg",
+   "alt": "Sail Rock in County Donegal offers great climbing",
+   "atributionURL": "http://donegalclimbing.ie/about/",
+   "attributionText": "img: Bren Whelan"
+ },
+ {
+   "climbId": 13,
+   "type": "crag",
+   "url": "img/topos/sail-rock/sail-rock-donegal-crag.jpg",
+   "alt": "Sail Rock in County Donegal offers great climbing",
+   "atributionURL": "http://donegalclimbing.ie/about/",
+   "attributionText": "img: Bren Whelan"
+ },
+ {
+   "climbId": 13,
+   "type": "topo",
+   "url": "img/topos/sail-rock/sail-rock-topo-ireland-donegal.jpg",
+   "alt": "Climb topo for sail rock Ireland",
+   "atributionURL": "https://uniqueascent.ie/sail_rock_guide",
+   "attributionText": "original img: Iain Miller"
+ },
+ {
+   "climbId": 14,
+   "type": "crag",
+   "url": "img/topos/chair/chair-ladder-crag-side.jpg",
+   "alt": "Chair Ladder Climbing Wall from the side",
+   "atributionURL": "https://www.geograph.org.uk/profile/180",
+   "attributionText": "img: David Medcalf"
+ },
+ {
+   "climbId": 14,
+   "type": "topo",
+   "url": "img/topos/chair/chair-ladder-topo-for-pegasus.jpg",
+   "alt": "Chair Ladder Route Topo for Pegasus",
+   "atributionURL": "http://www.cornishcamping.co.uk/photographs/cornish-seascapes/",
+   "attributionText": "img: Cornish Camping"
+ },
+ {
+   "climbId": 14,
+   "type": "tile",
+   "url": "img/tiles/chair-ladder.jpg",
+   "alt": "Chair Ladder Crag",
+   "atributionURL": "http://www.cornishcamping.co.uk/photographs/cornish-seascapes/",
+   "attributionText": "img: Cornish Camping"
+ }
+]};
