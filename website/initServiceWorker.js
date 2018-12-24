7e0dc25e9a593c7012dc43ba99671eea2c95e90d
Michel uncini <uncini.michel@gmail.com>
2018-12-24 17:41:04 +0100

    Added more staff to cache, maybe too many but it is an experiment :)

--- website/initServiceWorker.js
+++ website/initServiceWorker.js
@@ -5,12 +5,32 @@ var CACHE_WEBSITE = 'multi-pitch-cache-v1';
 
 var urlsToCache = [
     '/',
+    '/initServiceWorker.js',
+
     '/css/style.css',
     '/css/bootstrap-grid.css',
     '/css/owfont-regular.css',
+
+    '/data/climbs.js',
+    '/data/guidebooks.js',
+    '/data/imgs.js',
+    '/data/weather.js',
+
+    '/js/graph.js',
+    '/js/load-data.js',
     '/js/main.js',
+    '/js/offline-climb-cache.js',
+    '/components/climbCard.js',
+
+    '/img/download/download_no.svg',
+    '/img/download/download_yes.svg',
+
+    '/img/heros/old-man-of-stoer-hero-1800.jpg',
+    '/img/heros/old-man-of-stoer-hero-1800.webp',
+    '/img/heros/old-man-of-stoer-hero-2400.jpg',
+    '/img/heros/old-man-of-stoer-hero-2400.webp',
+    '/img/logo/mp-logo-white.png',
     '/img/favicon/favicon-32x32.png',
-    '/img/favicon/favicon-16x16.png',
     '/img/favicon/favicon-96x96.png',
     '/img/favicon/android-icon-192x192.png'
 ];
@@ -18,17 +38,26 @@ var urlsToCache = [
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_WEBSITE)
-            .then(cache => cache.addAll(urlsToCache))
+            .then(cache => {
+                cache.addAll(urlsToCache)
+            })
             .catch(err => console.error("An error occurred while trying to open the cache with error:", err))
     )
 });
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response ? response : fetch(event.request))
+        caches.open(CACHE_WEBSITE)
+            .then(function (cache) {
+                return cache.match(event.request).then(function (response) {
+                    return response || fetch(event.request).then(function (response) {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    });
+                });
+            })
             .catch(err => console.error("An error occurred while trying to cache match: ", event.request, "with error:", err))
-    );
+    )
 });
 
 
