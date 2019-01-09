fdec37d3b032b7ab81955408ca15814285aec41c
Michel uncini <uncini.michel@gmail.com>
2019-01-09 04:45:21 +0000

    Added Twitter sharing button ONLY on full page climb view no in the overview

--- website/components/climbPageTemplate.js
+++ website/components/climbPageTemplate.js
@@ -250,12 +250,19 @@ function climbCard(rootProject, climb, mapImg, cragImg, topoImg, guideBook, weat
                             <div class="row">
                                 <div class="col-sm">
                                     
-                                   <a href="whatsapp://send?text=${climb.routeName} on ${climb.cliff} https://multi-pitch.com/climbs/${climb.folderLocation}">
+                                   <a href="whatsapp://send?text=${climb.routeName} on ${climb.cliff} | https://multi-pitch.com/climbs/${climb.folderLocation}">
                                         <img 
                                         style="vertical-align: bottom; width: 30px; height: 30px;"
                                         src="${rootProject}img/icons/whatsapp.png" alt="Send WhatsApp" /> 
                                    </a>
         
+              
+                                 </div>
+                                 <div class="col-sm">
+                                    
+                                   <a class="twitter-share-button"
+                                      href="https://twitter.com/intent/tweet?text=${climb.routeName} on ${climb.cliff} |">
+                                    </a>
               
                                  </div>
                                 <div class="col-sm">
