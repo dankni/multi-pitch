c984f3a78f073bd83a6e5ae9254ab799f4fbd301
dankni <me@danknight.name>
2019-07-05 08:47:43 +0100

    added wintours leap climb

---
+++ website/data/topos/29.js
@@ -0,0 +1,12 @@
+var topoData = {
+    "image":"/img/topos/wye-valley/wintours-leap-crag.jpg",
+    "flag":"/img/flags/en.png",
+    "title":"Wintours Leap > Right Hand Route | HS 4b - 87m",
+    "belaySize":16,
+    "route":[[1801.0, 2008.4],[1825.0, 1941.4],[1807.0, 1818.4],[1788.0, 1744.4],[1814.0, 1640.4],[1832.0, 1599.4],[1809.0, 1520.4],[1847.0, 1407.4],[1867.0, 1326.4],[1866.0, 1224.4],[1863.0, 1127.4],[1854.0, 1056.4],[1815.0, 1048.4],[1806.0, 1029.4],[1757.0, 958.4],[1702.0, 866.4],[1673.0, 803.4],[1683.0, 739.4],[1668.0, 712.4],[1668.0, 643.4],[1657.0, 550.4],[1636.0, 478.4],[1584.0, 423.4],[1584.0, 363.4],[1584.0, 324.4] ],
+    "pitches":[
+        {"belayPosition":[1791,1765],"labelPosition":[1649,1849],"grade":"3c","height":"18m"},
+        {"belayPosition":[1856,1348],"labelPosition":[1687,1520],"grade":"4b","height":"21m"},
+        {"belayPosition":[1794,1012],"labelPosition":[1766,1174],"grade":"4b","height":"17m"},
+        {"belayPosition":[1581,331],"labelPosition":[1570,664],"grade":"4a","height":"30m"}],
+    "decent":[]};
