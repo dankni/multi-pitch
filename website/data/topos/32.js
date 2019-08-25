0b5bb7b2a26f9d132600bda60599d3e1dbffbcfd
dankni <me@danknight.name>
2019-08-25 10:01:04 +0100

    added Freyr in Belgium plus some error handdling on map weather as well as cretive commons license update.

---
+++ website/data/topos/32.js
@@ -0,0 +1,25 @@
+var topoData =  {
+    "image":"/img/topos/freyr/crag.jpg",
+    "flag":"/img/flags/be.png",
+    "title":"Freÿr > Le Merinos | S 4b - 100m",
+    "belaySize":24,
+    "route":[
+        [380,2418],[373,2294],[373,2136],[388,1884],[398,1768],[440,1644],[512,1561],[613,1353],
+        [662,1269],[718,1233],[783,1091],[840,1058],[872,993],[908,899],[942,869],[953,824],[1017,791],
+        [1082,793],[1128,779],[1172,678],[1195,604],[1238,549],[1280,471],[1323,391],[1392,358],
+        [1433,289],[1463,296]
+    ],
+    "pitches":[
+        {"belayPosition":[1440,289],"labelPosition":[1362,456],
+        "grade":"~30m 4b","height":"La Savonnette "},
+        {"belayPosition":[1098,786],"labelPosition":[963,1006],"grade":"","height":"~35m"},
+        {"belayPosition":[702,1239],"labelPosition":[635,1473],"grade":"3c","height":"~20m"},
+        {"belayPosition":[382,1946],"labelPosition":[465,2166],"grade":"3c","height":"~15m"}
+    ],
+    "decent":[
+        {"anchor":[null,null],"labelPosition":[800,650],
+        "path":[[1058,748],[1000,731],[1080,709]],"label":"End Le Merinos"},
+        {"anchor":[null,null],"labelPosition":[null,null],
+        "path":[[1577,263],[1677,238],[1793,211],[1905,234]],"label":""}
+    ]
+}
