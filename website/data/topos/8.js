481d434f3cba222c27228403be5d90756b266d19
dankni <me@danknight.name>
2019-09-19 20:50:41 +0100

    topo loading state plus better image and info for Lundy

---
+++ website/data/topos/8.js
@@ -0,0 +1,17 @@
+var topoData = {
+    "image":"/img/topos/lundy/crag.jpg",
+    "flag":"/img/flags/en.png",
+    "title":"Lundy > The Devils Slide | HS 4a - 117m",
+    "belaySize":24,
+    "route":[
+        [2588,2599],[2540,2436],[2493,2269],[2532,2128],[2513,1839],[2485,1651],
+        [2450,1404],[2345,1199],[2165,762],[2082,431],[2080,359],[1997,361],
+        [1928,367],[1885,267],[1716.0,186.7]
+    ],
+    "pitches":[
+    ],
+    "decent":[
+        {"anchor":[2515,1376],"labelPosition":[2587,1451.5],"path":[[2520,1394.5],[2540,1504]],"label":"Abseil in"},
+        {"anchor":[null,null],"labelPosition":[2402,763],"path":[[2415,699],[2335,719],[2358,796]],"label":"Scramble or abseil down"}
+    ]
+}
