bb598da6d9ffc5234276c7aa37b5122f3761517b
dankni <me@danknight.name>
2019-09-28 22:15:02 +0100

    added some general content updates and a better topo for idwal form our own images

---
+++ website/data/topos/7.js
@@ -0,0 +1,25 @@
+var topoData = {
+    "image":"/img/topos/idwal/crag.jpg",
+    "flag":"/img/flags/wa.png",
+    "title":"Cwm Idwal > Ordinary Route | D 3a - 140m",
+    "belaySize":24,
+    "route":[
+        [1965,2318],[1957,2015],[1979,1526],[2003,1081],[2060,967],[2108,918],[2085,867],[2058,817],
+        [2091,772],[2143,701],[2262,628],[2309,604],[2361,568],[2384,556],[2387,539],[2315,526],
+        [2245,491],[2246,479],[2329,448],[2392.0,441.5],[2441,384],[2466,373]
+    ],
+    "pitches":[
+        {"belayPosition":[2010,1068],"labelPosition":[2054,1414],"grade":"","height":"45m"},
+        {"belayPosition":[2310,604],"labelPosition":[2205,711],"grade":"","height":"45m"},
+        {"belayPosition":[2312,529],"labelPosition":[2422,554],"grade":"","height":"25m"},
+        {"belayPosition":[2465,369],"labelPosition":[2482,426],"grade":"","height":"25m"}
+    ],
+    "decent":[
+        {   "anchor":[null,null],
+            "labelPosition":[1851,273],
+            "path":[
+                [2375,384],[2260,399],[2139,417],[2075,380],[2073,352],[2060,338],[2035,343]
+            ],
+            "label":"Scramble up & over" }
+        ]
+    }
