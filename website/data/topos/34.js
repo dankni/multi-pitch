2e88d9a41ea2e4365ea77874f4affc2df3978881
dankni <me@danknight.name>
2019-12-20 13:12:52 +0000

    published old man of hoy

---
+++ website/data/topos/34.js
@@ -0,0 +1,26 @@
+var topoData = {
+    "image":"/img/topos/hoy/crag.jpg",
+    "flag":"/img/flags/sco.png",
+    "title":"Old Man of Hoy > Original Route | E1 5b - 137m",
+    "belaySize":17,
+    "route":[
+        [1067,1523],[1095,1435],[1149,1309],[1156,1306],[1158,1355],[1170,1368],[1203,1368],[1228,1301],
+        [1229,1240],[1225,1124],[1240,1089],[1247,1022],[1260,955],[1264,930],[1326,855],
+        [1327,795],[1345,738],[1375,710],[1383,659],[1382,563],[1376,507],[1390,310]
+    ],
+    "pitches":[
+        {"belayPosition":[1152,1307],"labelPosition":[1138,1430],"grade":"4b","height":"25m"},
+        {"belayPosition":[1243,1039],"labelPosition":[1271,1160],"grade":"5b","height":"35m"},
+        {"belayPosition":[1325,826],"labelPosition":[1320,935],"grade":"4b","height":"20m"},
+        {"belayPosition":[1379,666],"labelPosition":[1390.0,750],"grade":"4b","height":"35m"},
+        {"belayPosition":[1389,312],"labelPosition":[1423,442],"grade":"4c","height":"20m"}
+    ],
+    "decent":[
+        {"anchor":[null,null],"labelPosition":[null,null],"path":[[1055,1695],[1043,1650],[1043,1568]],"label":""},
+        {"anchor":[null,null],"labelPosition":[null,null],"path":[[1383,330],[1368,338],[1369,356]],"label":""},
+        {"anchor":[null,null],"labelPosition":[null,null],"path":[[1369,681],[1355,695]],"label":""},
+        {"anchor":[null,null],"labelPosition":[null,null],"path":[[1315,840],[1303,858]],"label":""},
+        {"anchor":[null,null],"labelPosition":[null,null],"path":[[1231,1052],[1220,1075]],"label":""},
+        {"anchor":[null,null],"labelPosition":[null,null],"path":[[1129,1303],[1107,1312],[1099,1344]],"label":""}
+    ]
+}
