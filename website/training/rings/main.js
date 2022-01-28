cf48df9dcf13835289973560c937bfc8cf216dd0
dankni <me@danknight.name>
2022-01-28 21:32:51 +0000

    crated a new training application based on metolious rock ring plan. also moved the trad game under theat folder

---
+++ website/training/rings/main.js
@@ -0,0 +1,215 @@
+const plan = [
+        {
+            "min" : 1,
+            "order" : 1,
+            "hold" : "jug.png",
+            "task" : "3 pull ups"
+        }, 
+        {
+            "min" : 2,
+            "order" : 1,
+            "hold" : "three_fingers.png",
+            "task" : "10 second bent-arm hang"
+        }, 
+        {
+            "min" : 2,
+            "order" : 2,
+            "hold" : "two_fingers.png",
+            "task" : "15 second straight-arm hang"
+        }, 
+        {
+            "min" : 3,
+            "order" : 1,
+            "hold" : "offset.png",
+            "task" : "2 pull ups"
+        }, 
+        {
+            "min" : 3,
+            "order" : 2,
+            "hold" : "offset2.png",
+            "task" : "2 pull ups"
+        }, 
+        {
+            "min" : 4,
+            "order" : 1,
+            "hold" : "four_fingers.png",
+            "task" : "20 second sit hang"
+        }, 
+        {
+            "min" : 4,
+            "order" : 2,
+            "hold" : "three_fingers.png",
+            "task" : "10 second straight arm hang"
+        },
+        {
+            "min" : 5,
+            "order" : 1,
+            "hold" : "four_fingers.png",
+            "task" : "5 pull ups"
+        }, 
+        {
+            "min" : 6,
+            "order" : 1,
+            "hold" : "three_fingers.png",
+            "task" : "20 second bent-arm hang"
+        }, 
+        {
+            "min" : 6,
+            "order" : 2,
+            "hold" : "two_fingers.png",
+            "task" : "10 second straight-arm hang"
+        }, 
+        {
+            "min" : 7,
+            "order" : 1,
+            "hold" : "four_fingers.png",
+            "task" : "15 second sit hang"
+        }, 
+        {
+            "min" : 7,
+            "order" : 2,
+            "hold" : "four_fingers.png",
+            "task" : "15 second straight-arm hang"
+        },
+        {
+            "min" : 8,
+            "order" : 1,
+            "hold" : "offset.png",
+            "task" : "10 second offset hang"
+        }, 
+        {
+            "min" : 8,
+            "order" : 2,
+            "hold" : "offset2.png",
+            "task" : "10 second offset hang"
+        },
+        {
+            "min" : 9,
+            "order" : 1,
+            "hold" : "four_fingers.png",
+            "task" : "20 second sit hang"
+        }, 
+        {
+            "min" : 10,
+            "order" : 1,
+            "hold" : "three_fingers.png",
+            "task" : "5 pull ups"
+        },
+        {
+            "min" : 10,
+            "order" : 2,
+            "hold" : "three_fingers.png",
+            "task" : "Striagt-arm hang to failure"
+        }
+    ];
+
+let started = false;
+let wakeLock = null;
+
+// Function that attempts to request a screen stay awake.
+const requestWakeLock = async () => {
+    try {
+        wakeLock = await navigator.wakeLock.request();
+        wakeLock.addEventListener('release', () => {
+            console.log('Screen Wake Lock released:', wakeLock.released);
+        });
+        console.log('Screen Wake Lock released:', wakeLock.released);
+    } catch (err) {
+        console.log(`${err.name}, ${err.message} - this probubly means the broswer doesn't support it`);
+    }
+};
+
+function setStarted(){
+    if(started === false) {
+        requestWakeLock();
+        started = true;
+        startTimer();
+        document.querySelector('button').innerHTML = 'Stop';
+    } else {
+        started = false;
+        stopTimer();
+        document.querySelector('button').innerHTML = 'Resume';
+    }
+    
+}
+
+function fullscreen(elem){
+if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
+
+function speak(inputTxt){
+    if (started === true){										// Chrome needs user activation
+    var utterThis = new SpeechSynthesisUtterance(inputTxt);
+    window.speechSynthesis.speak(utterThis);
+    }
+}
+
+function displayTask(min){
+    min = min + 1;
+    for(let i = 0; i < plan.length; i++){
+        // ToDo: use something more elegant here like a map? 
+        if((plan[i].min == min) && (plan[i].order == 1)) {
+            document.getElementById("first").style.display = "block";
+            document.getElementById("first_hold").src = "img/" + plan[i].hold;
+            document.getElementById("first_task").innerHTML = plan[i].task;
+        } 
+        if((plan[i].min == min) && (plan[i].order == 2)) {
+            document.getElementById("second").style.display = "block";
+            document.getElementById("second_hold").src = "img/" + plan[i].hold;
+            document.getElementById("second_task").innerHTML = plan[i].task;
+        } 
+    }
+}
+
+// To Count Elapsed time
+var min = 0;
+var sec = 0;
+var stoptime = true;
+
+function startTimer() {
+    displayTask(0);
+    if (stoptime == true) {
+        stoptime = false;
+        timerCycle();
+    }
+}
+function stopTimer() {
+    if (stoptime == false) {
+    stoptime = true;
+    }
+}
+
+function timerCycle() {
+    if (stoptime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+
+    sec = sec + 1;
+
+    if (sec == 60) {
+        min = min + 1;
+        sec = 0;
+        displayTask(min);
+    }
+
+    if (sec < 10 || sec == 0) {
+        sec = '0' + sec;
+    }
+    if (min < 10 || min == 0) {
+        min = '0' + min;
+    }
+
+    document.getElementById('elapsed').innerHTML = min + ':' + sec;
+    setTimeout("timerCycle()", 1000); // to test the app take set between 40 to 100 ^__^
+    }
+}
+
+function resetTimer() {
+    document.getElementById('elapsed').innerHTML = '00:00';
+}
