c66b074e874f4a9cc90c72560e9a9c48ac179a89
dankni <me@danknight.name>
2024-01-22 20:37:51 +0000

    added hold colour config to loft training app, also did some bug fixing with voice recognition and the reset

--- website/training/loft/main.js
+++ website/training/loft/main.js
@@ -5,19 +5,23 @@ let state = {
     "gearPlaced": 0,
     "beleysMade" : 0,
     "justPlaced" : false,
-    "anchorJustPlaced" : false
+    "anchorJustPlaced" : false,
+    "aborted" : false
 };
-
+// Global Options
+let colours;
+let limb = ["left arm", "right arm", "left leg", "right leg"];
+let gear = ["cam", "nut", "sling"];
 
 // When the HTML Document Object Model is loaded
 document.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById('primaryButton').addEventListener('click',setStarted);
     getOrSetColours();
+    checkTradMode();
 });
 
 // Get or set the colors
 function getOrSetColours(){
-    let colours;
     let defaulColours = ["white", "yellow", "orange", "grey", "red", "pink", "blue", "purple", "black", "green"]
     if(localStorage.getItem("colours")){
         colours = JSON.parse(localStorage.getItem("colours"));
@@ -41,7 +45,7 @@ function addColorSwitch(colour, status){
     if (status === 'On') {
         check = 'checked="checked"';
     }
-    
+
     let html = `
         <label class="switch-label">
 			<input type="checkbox" onchange="updateColors()" class="holds" data-color="${colour}" ${check}/>
@@ -63,9 +67,38 @@ function updateColors(){
             document.getElementById(holdSwitch.dataset.color + "Status").textContent = "Off";
         }
     });
-    localStorage.setItem("colours", JSON.stringify(newColours));
+    colours = newColours; // update global variable 
+    localStorage.setItem("colours", JSON.stringify(newColours)); // update local storage
 }
 
+// Manage Trad climbing mode
+function checkTradMode(){
+    let tradMode = false;
+    if(localStorage.getItem("tradMode")){
+        tradMode = JSON.parse(localStorage.getItem("tradMode"));
+    } else {
+        localStorage.setItem("tradMode", JSON.stringify(tradMode));
+    }
+    if(tradMode === true){
+        document.getElementById('gearDiv').style.display = 'inline-block';
+        document.getElementById('belaysDiv').style.display = 'inline-block';
+        document.getElementById('tradMode').checked = true;
+        document.getElementById('tradStatus').textContent = "On";
+    } else {
+        document.getElementById('gearDiv').style.display = 'none';
+        document.getElementById('belaysDiv').style.display = 'none';
+        document.getElementById('tradMode').checked = false;
+        document.getElementById('tradStatus').textContent = "Off";
+    }
+}
+function toggleTradMode(){
+    if(document.getElementById('tradMode').checked === true){
+        localStorage.setItem("tradMode", JSON.stringify(true));
+    } else {
+        localStorage.setItem("tradMode", JSON.stringify(false));
+    }
+    checkTradMode();
+}
 
 // kick of the first task and give chrome permission to use speechSynthesis
 function setStarted(){
@@ -76,9 +109,9 @@ function setStarted(){
         document.getElementById('primaryButton').innerHTML = '<i class="demo-icon icon-pause"></i>PAUSE';
         document.getElementById('reset').style.display = 'none';
     } else {
-        state.started = false;
-        stopTimer();
+        state.aborted = true;
         recognition.abort();
+        stopTimer();
         document.getElementById('primaryButton').innerHTML = '<i class="demo-icon icon-play"></i>RESUME';
         document.getElementById('reset').style.display = 'inline-block'
 
@@ -89,7 +122,7 @@ function setStarted(){
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
-const grammar = '#JSGF V1.0; grammar word;'// public <word> = ' + word.join(' | ') + ' ;'
+var grammar = '#JSGF V1.0; grammar replies; public <reply> = next | compleated | done;';
 var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
 
@@ -99,85 +132,87 @@ recognition.continuous = false; // keeps service open to allow multiple words if
 recognition.lang = 'en-GB';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
-let movesMade = 0;
-let gearPlaced = 0;
-let beleysMade = 0;
-let justPlaced = false;
-let anchorJustPlaced = false;
 
 function startSoon (){
     state.started ? console.log("already running") : recognition.start();
-    console.log("func startSoon");
 }
+recognition.onstart = function () {
+    state.started = true;
+    state.aborted = false;
+};
 
 recognition.onresult = function(event) {
-    var command = event.results[0][0].transcript;				// if continuous is true the first array needs iteration
-    console.log(command + ' Confidence: ' + event.results[0][0].confidence);
+    var command = event.results[0][0].transcript;		// if continuous is true the first array needs iteration
+//    console.log(command + ' Confidence: ' + event.results[0][0].confidence);
     processResults(command);
 }
-
-recognition.onspeechend = function() {state.started = false}
-recognition.onstart = function () {state.started = true};
 
 recognition.onnomatch = function(event) {
     console.log('I didn\'t recognise that command.');
     startSoon();
 }
 recognition.onerror = function(event) {
+    // "no-speech" is time out
     console.log('Error occurred in recognition: ' + event.error);
     startSoon();
 }
 recognition.onend = function () {
-    startSoon();
+    state.started = false;
+    if(state.aborted === false){
+        startSoon(); // restarts recognition
+    }
 };
 
 function processResults(command){
     if(command.includes('next')){
         speak(chooseTask());
-    } else {
-        speak("I didn't understand");
     }
-//    recognition.abort();
+    startSoon();
 }
 
 function chooseTask(){
-    // ToDo: Make all these configurable 
-    let limb = ["left arm", "right arm", "left leg", "right leg"];
-    let gear = ["cam", "nut", "sling"];
-    
     // todo - make this more readable 
-    if(movesMade % 8 === 0 && movesMade !== 0 && justPlaced !== true && anchorJustPlaced !== true){
-        if(gearPlaced % 4 === 0 && gearPlaced !== 0 && anchorJustPlaced !== true){
-            document.getElementById('color').style.backgroundColor = "white";
-            let belayMsg = "Build a belay and take a rest";
-            document.getElementById('task').innerHTML = belayMsg;
-            anchorJustPlaced = true;
-            beleysMade += 1;
-            document.getElementById('belays').innerHTML = beleysMade;
-            return belayMsg;
+    if(JSON.parse(localStorage.getItem('tradMode')) === true){
+        if(state.movesMade % 8 === 0 && state.movesMade !== 0 && state.justPlaced !== true && state.anchorJustPlaced !== true){
+            if(state.gearPlaced % 4 === 0 && state.gearPlaced !== 0 && state.anchorJustPlaced !== true){
+                document.getElementById('color').style.backgroundColor = "white";
+                let belayMsg = "Build a belay and take a rest";
+                document.getElementById('task').innerHTML = belayMsg;
+                state.anchorJustPlaced = true;
+                state.beleysMade += 1;
+                document.getElementById('belays').innerHTML = state.beleysMade;
+                return belayMsg;
+            } else {
+                document.getElementById('color').style.backgroundColor = "white";
+                let gearMsg = "Place a " + gear[Math.floor(Math.random() * gear.length)];
+                document.getElementById('task').innerHTML = gearMsg;
+                state.gearPlaced += 1;
+                document.getElementById('gear').innerHTML = state.gearPlaced;
+                state.justPlaced = true;
+                return gearMsg;
+            }
         } else {
-            document.getElementById('color').style.backgroundColor = "white";
-            let gearMsg = "Place a " + gear[Math.floor(Math.random() * gear.length)];
-            document.getElementById('task').innerHTML = gearMsg;
-            gearPlaced += 1;
-            document.getElementById('gear').innerHTML = gearPlaced;
-            justPlaced = true;
-            return gearMsg;
+            return randomMove();
         }
     } else {
-        let randomLimb = limb[Math.floor(Math.random() * limb.length)];
-        let randomColor = color[Math.floor(Math.random() * color.length)];
-        movesMade += 1;
-        justPlaced = false;
-        anchorJustPlaced = false;
-        document.getElementById('moves').innerHTML = movesMade;
-        document.getElementById('color').style.backgroundColor = randomColor;
-        document.getElementById('task').innerHTML = randomLimb + " to " + randomColor;
-        return randomLimb + " to " + randomColor;
+        return randomMove();
     }
 }
 
+function randomMove(){
+    let randomLimb = limb[Math.floor(Math.random() * limb.length)];
+    let randomColor = colours[Math.floor(Math.random() * colours.length)];
+    state.movesMade += 1;
+    state.justPlaced = false;
+    state.anchorJustPlaced = false;
+    document.getElementById('moves').innerHTML = state.movesMade;
+    document.getElementById('color').style.backgroundColor = randomColor;
+    document.getElementById('task').innerHTML = randomLimb + " to " + randomColor;
+    return randomLimb + " to " + randomColor;
+}
+
 // To Count Elapsed time
+let hr = 0;
 let min = 0;
 let sec = 0;
 let stoptime = true;
@@ -221,71 +256,7 @@ function timerCycle() {
     if (stoptime == false) {
     sec = parseInt(sec);
     min = parseInt(min);
-
-    sec = sec + 1;
-    if (sec === 1){
-        background("");// default
-    }
-    if (sec < 10 || sec == 0) {
-        sec = '0' + sec;
-    }
-    if (min < 10 || min == 0) {
-        min = '0' + min;
-    }
-    if (min === 60 && wakeLock != null){
-        //don't force the screen to stay awake anymore 
-        wakeLock.release()
-        .then(() => {
-            wakeLock = null;
-        });
-    }
-
-    document.getElementById('elapsed').innerHTML = min + ':' + sec;
-        debug ? setTimeout("timerCycle()", 100) : setTimeout("timerCycle()", 1000);
-    }
-}
-
-function reset() {
-    document.getElementById('elapsed').innerHTML = '00:00';
-    min = 0;
-    sec = 0;
-    stoptime = true;
-    document.getElementById("first").style.display = "none";
-    document.getElementById("second").style.display = "none";
-    document.getElementById("reset").style.display = "none";
-    document.querySelector('button').innerHTML = '<i class="demo-icon icon-play"></i>START';
-    introRun = false;
-    if(document.getElementById("endingDiv").style.display === "block"){
-        document.getElementById("endingDiv").style.display = "none";
-    }
-}
-
-/*/ To Count Elapsed time... why not eh
-const timer = document.getElementById('elapsed');
-
-var hr = 0;
-var min = 0;
-var sec = 0;
-var stoptime = true;
-
-function startTimer() {
-    if (stoptime == true) {
-        stoptime = false;
-        timerCycle();
-    }
-}
-function stopTimer() {
-    if (stoptime == false) {
-    stoptime = true;
-    }
-}
-
-function timerCycle() {
-    if (stoptime == false) {
-    sec = parseInt(sec);
-    min = parseInt(min);
     hr = parseInt(hr);
-
     sec = sec + 1;
 
     if (sec == 60) {
@@ -298,23 +269,51 @@ function timerCycle() {
         sec = 0;
     }
 
+    if (sec === 1){
+        background("");// default
+    }
     if (sec < 10 || sec == 0) {
-        sec = '0' + sec;
+        sec = '0' + sec; // so its 05 sec not just 5
     }
     if (min < 10 || min == 0) {
         min = '0' + min;
     }
-    if (hr < 10 || hr == 0) {
-        hr = '0' + hr;
+
+    if (min === 60 && wakeLock != null){
+        //don't force the screen to stay awake anymore 
+        wakeLock.release()
+        .then(() => {
+            wakeLock = null;
+        });
     }
 
-    timer.innerHTML = hr.substring(1, 2) + ':' + min + ':' + sec;
-
-    setTimeout("timerCycle()", 1000);
+    document.getElementById('elapsed').innerHTML = hr + ':' + min + ':' + sec;
+    debug ? setTimeout("timerCycle()", 100) : setTimeout("timerCycle()", 1000);
     }
 }
 
-function resetTimer() {
-    timer.innerHTML = '00:00:00';
+function reset() {
+    hr = 0;
+    min = 0;
+    sec = 0;
+    stoptime = true;
+    document.getElementById('color').style.backgroundColor = '';
+    document.getElementById('task').textContent = '';
+    document.getElementById("reset").style.display = "none";
+    document.querySelector('button').innerHTML = '<i class="demo-icon icon-play"></i>START';
+    introRun = false;
+    state = {  
+        "started" : false,
+        "listening" : false,
+        "movesMade" : 0,
+        "gearPlaced": 0,
+        "beleysMade" : 0,
+        "justPlaced" : false,
+        "anchorJustPlaced" : false,
+        "aborted" : true
+    };
+    document.getElementById('elapsed').innerHTML = '0:00:00';
+    document.getElementById('moves').textContent = '0';
+    document.getElementById('belays').textContent = '0';
+    document.getElementById('gear').textContent = '0';
 }
-*/
