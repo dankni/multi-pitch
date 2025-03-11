ac0c14c92c6247446e057cbe46511013101f4fa2
dankni <me@danknight.name>
2025-03-11 20:44:10 +0000

    reverting server for CircleCI and adding my own as a seperate file.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/*const port = 9000;
+const port = 9000;
 const static = require('node-static');
 const http = require('http');
 const file = new static.Server('./website');
@@ -29,79 +29,4 @@ var promiseStart = start();
 module.exports = {
     promiseStart,
     stop
-};*/
-
-import { readFile, accessSync, constants } from 'fs';
-import { createServer } from 'http';
-import { join, normalize, resolve, extname } from 'path';
-
-const port = 9000;
-const directoryName = './website';
-
-const types = {
-  html: 'text/html',
-  css: 'text/css',
-  js: 'application/javascript',
-  png: 'image/png',
-  jpg: 'image/jpeg',
-  jpeg: 'image/jpeg',
-  webp: 'image/webp',
-  gif: 'image/gif',
-  webm: 'video/webm',
-  mp4: 'video/mp4',
-  svg: 'image/svg+xml',
-  woff: 'font/woff',
-  woff2: 'font/woff2',
-  json: 'application/json',
-  xml: 'application/xml',
 };
-
-const root = normalize(resolve(directoryName));
-
-const server = createServer((req, res) => {
-  console.log(`${req.method} ${req.url}`);
-
-  let extension = extname(req.url).slice(1).split('?')[0];
-  const type = extension ? types[extension] : types.html;
-  const supportedExtension = Boolean(type);
-
-  if (!supportedExtension) {
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end('404: File not found');
-    return;
-  }
-
-  let fileName = req.url.split('?')[0];
-  if (req.url === '/') fileName = 'index.html';
-  else if (!extension) {
-    try {
-      accessSync(join(root, req.url + '.html'), constants.F_OK);
-      fileName = req.url + '.html';
-    } catch (e) {
-      fileName = join(req.url, 'index.html');
-    }
-  }
-
-  const filePath = join(root, fileName);
-  const isPathUnderRoot = normalize(resolve(filePath)).startsWith(root);
-
-  if (!isPathUnderRoot) {
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end('404: File not found');
-    return;
-  }
-
-  readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.end('404: File not found');
-    } else {
-      res.writeHead(200, { 'Content-Type': type });
-      res.end(data);
-    }
-  });
-});
-
-server.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
